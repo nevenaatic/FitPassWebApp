9d24a1c43f70479875ecc98fdebe2d94311fc481
nevenaatic <atic.ra67.2018@uns.ac.rs>
2022-06-20 18:08:52 +0200

    gitignore and method for search

---
+++ EliminacioniREST/src/dto/SearchObjectsDto.java
@@ -0,0 +1,14 @@
+package dto;
+
+import enums.PlaceType;
+
+public class SearchObjectsDto {
+
+	
+	public String name; 
+	public String city; //naziv grada
+	public PlaceType type; 
+	public String grade; 
+	
+	
+}
